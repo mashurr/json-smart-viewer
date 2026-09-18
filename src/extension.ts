8294567e8fb4c5013617591446516ca989462678
mashurr <mashur19122000@gmail.com>
2026-09-18 04:30:28 -0400

    Generate webview nonces with crypto

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as crypto from 'crypto';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -127,12 +128,7 @@ function generateJsonHtml(jsonData: unknown): string {
 }
 
 function getNonce(): string {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv8901';
-    for (let i = 0; i < 32; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+    return crypto.randomBytes(16).toString('hex');
 }
 
 export function deactivate() {}
